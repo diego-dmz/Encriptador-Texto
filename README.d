b07120687da061eb3662ca6710114593f5203bc6
Diego Dominguez <godiepxndozo@gmail.com>
2024-08-01 22:55:57 -0500

    readme

--- README.d
+++ README.d
@@ -1,17 +1,19 @@
-# Aplicación de Encriptación de Texto
+# **Aplicación de Encriptación de Texto**
 
-Una aplicación web que permite encriptar y desencriptar texto utilizando una técnica de sustitución simple. Además, proporciona funcionalidades para filtrar el texto, copiar el texto encriptado al portapapeles y mostrar mensajes temporales en lugar de alertas.
+Una **aplicación web** que permite encriptar y desencriptar texto utilizando una técnica de sustitución simple. La aplicación también incluye funcionalidades para filtrar el texto, copiar el texto encriptado al portapapeles y mostrar mensajes temporales en lugar de alertas.
 
-## Funcionalidades
+## **Funcionalidades**
 
-- **Encriptar Texto**: Convierte el texto ingresado en una forma encriptada utilizando sustituciones específicas.
+- **Encriptar Texto**: Convierte el texto ingresado en una forma encriptada mediante sustituciones específicas.
 - **Desencriptar Texto**: Convierte el texto encriptado de vuelta a su forma original.
 - **Filtrar Texto**: Elimina caracteres no deseados del texto.
 - **Copiar Texto**: Copia el texto encriptado al portapapeles.
 - **Mensajes Temporales**: Muestra mensajes temporales para notificaciones en lugar de alertas.
 
-## Tecnologías Utilizadas
+## **Tecnologías Utilizadas**
 
-- HTML
-- CSS
-- JavaScript
+- **HTML**
+- **CSS**
+- **JavaScript**
+
+
